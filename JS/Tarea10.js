079a2d1549bff7f8c43fc4f6560c67c7d9fa1b66
qpepe2 <qpepe2@gmail.com>
2024-05-17 20:28:45 -0300

    commit 11

---
+++ JS/Tarea10.js
@@ -0,0 +1,24 @@
+let nombre = "Carlo";
+let apellido = "Perex";
+
+let nomApell= nombre + " " + apellido;
+console.log ("El nombre y apellido es " + nomApell);
+
+
+let numero1 = 11;
+let numero2 = 22;
+let suma1 = numero1 + numero2;
+
+console.log ("El primer numero es " + numero1 + ", el segundo número es "+ numero2 + ", y su suma da "+ suma1);
+
+let suma2 = suma1 + 100
+
+console.log ("La adición de 100 a la suma de los 2 primeros número da " + suma2)
+
+let varBooleana = true;
+let suma3 = suma2 * varBooleana;
+let suma4 = suma2 + nomApell;
+
+console.log ("La multiplicación de una variable numérica y una variable booleana da "+ suma3)
+console.log ("La suma de una variable numerica y una variable string da "+ suma4)
+
