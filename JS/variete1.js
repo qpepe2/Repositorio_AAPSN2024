7694e2c30696d45eb18c8f3550776e5f07b2d24c
qpepe2 <qpepe2@gmail.com>
2024-05-28 19:38:46 -0300

    commit 19

--- JS/variete1.js
+++ JS/variete1.js
@@ -1,12 +1,14 @@
 let edad = prompt("Por favor, indicar su edad antes de continuar:");
 
-while (isNaN(edad)){
+while (isNaN(edad))
+    {
     edad = prompt("Por favor, indicar su edad antes de continuar:");
-}
-if (edad>=18){
+    }
+if (edad>=18)
+    {
     alert ( "¡Bienvenid@ a la web de Variete!" );
-}
-else{
+    }
+else
+    {
     alert ( "ADVERTENCIA: Ud. es menor de edad, se recomienda ei acompañamento de una persona mayor");
-}
-
+    }
