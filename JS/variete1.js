4af344b98ec58e6dc8cb23751f527195a64a07a2
qpepe2 <qpepe2@gmail.com>
2024-05-28 20:01:42 -0300

    commit 20

--- JS/variete1.js
+++ JS/variete1.js
@@ -1,14 +1,17 @@
-let edad = prompt("Por favor, indicar su edad antes de continuar:");
+let nro = prompt("Por favor, indicar su edad antes de continuar");
 
-while (isNaN(edad))
-    {
-    edad = prompt("Por favor, indicar su edad antes de continuar:");
+function validacion(nro) {
+    nro = parseInt(nro); //PARSEINT convierte el string en un número entero
+
+    while (isNaN(nro)) {//isNaN devuelve true si el valor no es un número
+        nro = parseInt(prompt("Por favor, indicar su edad antes de continuar")); //Si no es un número, se vuelve a pedir el ingreso
     }
-if (edad>=18)
-    {
-    alert ( "¡Bienvenid@ a la web de Variete!" );
+
+    if (nro >= 18) { //
+        alert("Bienvenid@ a la web Variete!");
+    } else {
+        alert("ADVERTENCIA: Ud. es menor de edad, se recomienda el acompañamiento de una persona mayor");
     }
-else
-    {
-    alert ( "ADVERTENCIA: Ud. es menor de edad, se recomienda ei acompañamento de una persona mayor");
-    }
+}
+
+validacion(nro);
